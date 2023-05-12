2a6d27fddbb96abf077622487330449b5ffee0e7
sooto selim <adewaleselim6@gmail.com>
2023-05-12 13:29:48 +0100

    working 12

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,45 +1,76 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-const quote = {
-  initial: {
-    opacity: 1,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      delay: 0.5,
-      staggerChildren: 0.08,
-    },
-  },
-};
-export const singleWord = {
-  initial: {
-    opacity: 0,
-    y: 50,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 1,
-    },
-  },
-};
 const AnimatedText = ({ text, className = "" }) => {
+  const sectionRef = useRef(null);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            controls.start("animate");
+          } else {
+            controls.start("initial");
+          }
+        });
+      },
+      { threshold: 0.5 } // Adjust the threshold value as needed
+    );
+
+    if (sectionRef.current) {
+      sectionObserver.observe(sectionRef.current);
+    }
+
+    return () => {
+      sectionObserver.disconnect();
+    };
+  }, [controls]);
+
+  const quote = {
+    initial: {
+      opacity: 1,
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.08,
+      },
+    },
+  };
+
+  const singleWord = {
+    initial: {
+      opacity: 0,
+      y: 50,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+    <div
+      ref={sectionRef}
+      className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden"
+    >
       <motion.h1
-        className={`${className} inline-block w-full text-dark  text-8xl `}
+        className={`${className} inline-block w-full text-dark text-8xl `}
         variants={quote}
         initial="initial"
-        animate="animate"
+        animate={controls}
       >
         {text.split(" ").map((word, i) => (
           <motion.span
             key={word + i}
-            className="inline-block "
+            className="inline-block"
             variants={singleWord}
           >
             {word}&nbsp;
