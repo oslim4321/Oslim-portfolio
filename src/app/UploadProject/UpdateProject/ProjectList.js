49ec4b461ff1d5ecb9ae712070bddad877bdb791
sooto selim <adewaleselim6@gmail.com>
2023-06-03 15:42:47 +0100

    working 1

---
+++ src/app/UploadProject/UpdateProject/ProjectList.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+      <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+        <table className="min-w-full leading-normal">
+          <thead>
+            <tr>
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Project
+              </th>
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Student Name(optional)
+              </th>
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Created at
+              </th>
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Edit
+              </th>
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Delete
+              </th>
+
+              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0 w-10 h-10">
+                    <img
+                      className="w-full h-full rounded-full"
+                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                      Vera Carpenter
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                <p className="text-gray-900 whitespace-no-wrap">Admin</p>
+              </td>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                <p className="text-gray-900 whitespace-no-wrap">Jan 21, 2020</p>
+              </td>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm cursor-pointer border flexd justify-center items-center">
+                <p className="text-gray-900 whitespace-no-wrap ">Edit</p>
+              </td>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm cursor-pointer border flexd justify-center items-center">
+                <p className="text-gray-900 whitespace-no-wrap ">delete</p>
+              </td>
+              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                  <span
+                    aria-hidden
+                    className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                  ></span>
+                  <span className="relative">Activo</span>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+          <span className="text-xs xs:text-sm text-gray-900">
+            Showing 1 to 4 of 50 Entries
+          </span>
+          <div className="inline-flex mt-2 xs:mt-0">
+            <button className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+              Prev
+            </button>
+            <button className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+              Next
+            </button>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
--- src/app/UploadProject/UpdateProject/SelectProjectCat.js
+++ src/app/UploadProject/UpdateProject/SelectProjectCat.js
@@ -1,16 +1,29 @@
-import React from "react";
+"use client";
+import { buttons as projectCategory } from "@/lib/utilty/arrayList";
+import React, { useState } from "react";
 
 const SelectProjectCat = () => {
+  const [category, setcategory] = useState("Client works");
+  console.log(category);
   return (
     <div>
-      {" "}
+      <div>
+        <h2 className="text-2xl font-semibold leading-tight">{category} hy</h2>
+      </div>
       <div className="my-2 flex sm:flex-row flex-col">
         <div className="flex flex-row mb-1 sm:mb-0">
           <div className="relative">
             <select className="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-              <option>All</option>
-              <option>Active</option>
-              <option>Inactive</option>
+              {projectCategory.map((cat) => (
+                <option
+                  key={cat.category}
+                  onClick={() => setcategory(cat.text)}
+                >
+                  {cat.text}
+                </option>
+              ))}
+              {/* <option>Active</option>
+              <option>Inactive</option> */}
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg
