b62c105fd511a543289f8900f9df6b67b6b2b6f0
sooto selim <adewaleselim6@gmail.com>
2023-08-25 14:20:46 +0100

    fix the issue where by lest change where not been fetched when the projext page loads

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -2,10 +2,11 @@ import TransitionEffect from "@/src/components/TransitionEffect";
 import ProjectParent from "./ProjectParent";
 // import { db } from "@/lib/utilty/firebase";
 // import { collection, getDocs, query, where } from "firebase/firestore";
-import { getData } from "@/lib/QueryFirebase";
+import { fetchItemsByCategory, getData } from "@/lib/QueryFirebase";
 
 const page = async () => {
-  const data = await getData();
+  // const data = await getData();
+  const data = await fetchItemsByCategory("client-work");
 
   const styleOverflow = {
     overflow: "hidden",
